761b990f7ff23d2c25f8e0023fa12843382046d9
tcu38 <natasa.trkulja@uni-ulm.de>
2024-05-29 09:55:33 +0200

    updated error msgs to not include a dot and updated Opinion.go

--- cmd/main.go
+++ cmd/main.go
@@ -9,8 +9,11 @@ import (
 func main() {
 
 	op1, _ := subjectivelogic.NewOpinion(0.2,0.3,0.5,0.5)
-	fmt.Println(op1)
+	op2, _ := subjectivelogic.NewOpinion(0.1,0.2,0.7,0.5)
+	fmt.Println(op1, op2)
 	
+	op3, _ := subjectivelogic.Addition(&op1, &op2)
+	fmt.Println(op3)
 
 }
 
