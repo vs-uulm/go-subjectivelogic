fc701c6abd11965a015e2b9f84abc2cda572960d
tcu38 <natasa.trkulja@uni-ulm.de>
2024-05-31 11:14:52 +0200

    updating the avg fusion op code

--- pkg/subjectivelogic/OperatorAveragingFusion.go
+++ pkg/subjectivelogic/OperatorAveragingFusion.go
@@ -24,12 +24,10 @@ func AveragingFusion(opinion1 *Opinion, opinion2 *Opinion) (Opinion, error) {
 	}
 
 	b1 := opinion1.belief
-	d1 := opinion1.disbelief
 	u1 := opinion1.uncertainty
 	a1 := opinion1.baseRate
 
 	b2 := opinion2.belief
-	d2 := opinion2.disbelief
 	u2 := opinion2.uncertainty
 	a2 := opinion2.baseRate
 
@@ -37,18 +35,21 @@ func AveragingFusion(opinion1 *Opinion, opinion2 *Opinion) (Opinion, error) {
 	d := -1.0
 	u := -1.0
 	a := -1.0
-	if u1 == 0 && u2 == 0 {
-		b = 0.5 * (b1 + b2)
-		d = 0.5 * (d1 + d2)
-		u = 0
-		a = 0.5 * (a1 + a2)
 
-	} else {
+	if u1 != 0 || u2 != 0 {
+
 		b = (b1*u2 + b2*u1) / (u1 + u2)
-		d = (d1*u2 + d2*u1) / (u1 + u2)
 		u = 2 * u1 * u2 / (u1 + u2)
 		a = (a1 + a2) / 2
+
+	} else {
+
+		b = 0.5 * (b1 + b2)
+		u = 0
+		a = 0.5 * (a1 + a2)
 	}
+
+	d = 1 - b - u
 
 	return NewOpinion(b, d, u, a)
 }
